43958dde0c1ca32a5b4fbc1952334d9d6f3a5df3
anuuyu <yusrandante@gmail.com>
2022-06-20 07:40:49 +0800

    :rocket: tailwindCSS start

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -2,6 +2,9 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
+      <h1 className="text-3xl text-center font-bold underline">
+      Hello world!
+    </h1>
       <ul>
         <li>
           <a
